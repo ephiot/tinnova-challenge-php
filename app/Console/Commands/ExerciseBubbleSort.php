0ce813823a06ffe915583fb7ed01adb7be009baa
Mauricio Queiroz <ephiot@gmail.com>
2021-06-29 01:02:26 -0300

    Renamed Exercise2 => ExerciseBubbleSort

--- app/Console/Commands/ExerciseBubbleSort.php
+++ app/Console/Commands/ExerciseBubbleSort.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Helpers\BubbleSort;
 use Illuminate\Console\Command;
 
-class Exercise1 extends Command
+class ExerciseBubbleSort extends Command
 {
     /**
      * The name and signature of the console command.
